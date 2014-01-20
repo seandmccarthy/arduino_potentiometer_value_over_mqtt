562fecc87fbc2d547c1ceb1e1df6797d6710d8c7
Sean McCarthy <sean@mobonlinebusiness.com.au>
2014-01-20 20:16:24 +1100

    Added firehose mode button and LED

--- potentiometer_value_over_mqtt.ino
+++ potentiometer_value_over_mqtt.ino
@@ -3,70 +3,99 @@
 #include <PubSubClient.h>
 
 int level = 0;
-int data_pin = 2;
-int latch_pin = 3;
-int clock_pin = 4;
-int diffMode = 0;
+int dataPin = 2;
+int latchPin = 3;
+int clockPin = 4;
+int buttonPin = 5;
+int firehoseLedPin = 6;
 
-byte mac[]    = {  0xDE, 0xED, 0xBA, 0xFE, 0xFE, 0xED };
-byte server[] = { 10, 0, 1, 6 };
-byte ip[]     = { 10, 0, 1, 8 };
-
-EthernetClient ethClient;
-PubSubClient client(server, 1883, callback, ethClient);
-
-boolean debug = true;
+int firehose = LOW;
+long lastDebounceTime = 0;   // the last time the output pin was toggled
+long debounceDelay = 50;     // the debounce time; increase if the output flickers
+int firehoseLedState = HIGH; // the current state of the output pin
+int buttonState;
+int lastButtonState = HIGH;  // the previous reading from the input pin
 
 int last_output = 1;
-
 int led_map[] = {0x01, 0x03, 0x07, 0x0F, 0x1F, 0x3F, 0x7F, 0xFF};
+
+byte macAddress[]    = {  0xDE, 0xED, 0xBA, 0xFE, 0xFE, 0xED };
+byte server[] = { 10, 0, 1, 6 };
+
+EthernetClient ethClient;
+PubSubClient client(server, 1883, mqttCallback, ethClient);
+int checkFirehoseMode();
+int levelToOutput(int level);
+
+void mqttCallback(char* topic, byte* payload, unsigned int length) {
+  // handle message arrived
+}
+
+void setup() {
+  pinMode(dataPin, OUTPUT);
+  pinMode(latchPin, OUTPUT);
+  pinMode(clockPin, OUTPUT);
+  pinMode(buttonPin, INPUT);
+  pinMode(firehoseLedPin, OUTPUT);
+
+  Serial.begin(38400);
+  Ethernet.begin(macAddress); // IP via DHCP
+  client.connect("arduinoClient");
+}
+
+void loop() {
+  firehose = checkFirehoseMode();
+  
+  level = analogRead(A0);
+  int output = levelToOutput(level);
+  
+  char message[5];
+  
+  if (firehose == HIGH) {
+    sprintf(message, "%d", level); 
+    client.publish("readings", message);
+  }
+    
+  if (output != last_output) {
+    if (firehose == LOW) {
+      sprintf(message, "%d", output); 
+      client.publish("readings", message);
+    }
+    digitalWrite(latchPin, LOW);
+    shiftOut(dataPin, clockPin, MSBFIRST, output);
+    digitalWrite(latchPin, HIGH);
+  }
+  last_output = output;
+
+  delay(50);
+}
 
 int levelToOutput(int level) {
   int index = (int)level/128;
   return (int)led_map[index];
 }
 
-void callback(char* topic, byte* payload, unsigned int length) {
-  // handle message arrived
-}
+int checkFirehoseMode() {
+  int reading = digitalRead(buttonPin);
 
-void setup() {
-  pinMode(data_pin, OUTPUT);
-  pinMode(latch_pin, OUTPUT);
-  pinMode(clock_pin, OUTPUT);
-  Serial.begin(38400);
-  Ethernet.begin(mac, ip);
-  client.connect("arduinoClient");
-}
-
-void log(String message) {
-  if (debug) {
-    Serial.println(message);
-  }
-}
-
-void loop() {
+  if (reading != lastButtonState) {
+    // reset the debouncing timer
+    lastDebounceTime = millis();
+  } 
   
-  level = analogRead(A0);
-  int output = levelToOutput(level);
-  char message[4];
-  
-  if diffMode == 0) {
-    sprintf(message, "%d", level); 
-    client.publish("readings", message);
-  end
-    
-  if (output != last_output) {
-    //log(String("Level changed from ") + last_output + " to " + output);
-    if (diffMode == 1)
-      sprintf(message, "%d", output); 
-      client.publish("readings", message);
+  if ((millis() - lastDebounceTime) > debounceDelay) {
+    if (reading != buttonState) {
+      buttonState = reading;
+
+      // only toggle the LED if the new button state is HIGH
+      if (buttonState == HIGH) {
+        firehoseLedState = !firehoseLedState;
+      }
     }
-    digitalWrite(latch_pin, LOW);
-    shiftOut(data_pin, clock_pin, MSBFIRST, output);
-    digitalWrite(latch_pin, HIGH);
   }
-  last_output = output;
+  
+  digitalWrite(firehoseLedPin, firehoseLedState);
+  lastButtonState = reading;
 
-  delay(100);
+  return firehoseLedState;
 }
