7faee5029e4fc46debdc2445fa0c53441f3ac7f1
Sean McCarthy <sean@mobonlinebusiness.com.au>
2014-01-21 20:33:23 +1100

    Cope with a noisy POT. Reconnect to mqtt broker if needed.

--- potentiometer_value_over_mqtt.ino
+++ potentiometer_value_over_mqtt.ino
@@ -15,8 +15,9 @@ long debounceDelay = 50;
 int firehoseLedState = LOW;
 int buttonState;
 int lastButtonState = LOW;  // the previous reading from the input pin
+int errorFactor = 20;
 
-int last_output = 1;
+int lastLevel = 1;
 int led_map[] = {0x01, 0x03, 0x07, 0x0F, 0x1F, 0x3F, 0x7F, 0xFF};
 
 byte macAddress[]    = {  0xDE, 0xED, 0xBA, 0xFE, 0xFE, 0xED };
@@ -26,6 +27,7 @@ EthernetClient ethClient;
 PubSubClient client(server, 1883, mqttCallback, ethClient);
 int checkFirehoseMode();
 int levelToOutput(int level);
+void checkConnection();
 
 void mqttCallback(char* topic, byte* payload, unsigned int length) {
   // handle message arrived
@@ -46,10 +48,9 @@ void setup() {
 
 void loop() {
   firehose = checkFirehoseMode();
+  checkConnection();
   
-  level = analogRead(A0);
-  int output = levelToOutput(level);
-  
+  level = analogRead(A0);  
   char message[5];
   
   if (firehose == HIGH) {
@@ -57,18 +58,24 @@ void loop() {
     client.publish("readings", message);
   }
     
-  if (output != last_output) {
+  if (abs(level - lastLevel) > errorFactor) {
     if (firehose == LOW) {
-      sprintf(message, "%d", output); 
+      sprintf(message, "%d", level); 
       client.publish("readings", message);
     }
     digitalWrite(latchPin, LOW);
-    shiftOut(dataPin, clockPin, MSBFIRST, output);
+    shiftOut(dataPin, clockPin, MSBFIRST, levelToOutput(level));
     digitalWrite(latchPin, HIGH);
   }
-  last_output = output;
+  lastLevel = level;
 
   delay(50);
+}
+
+void checkConnection() {
+  if (!client.connected()) {
+    client.connect("arduinoClient");
+  }
 }
 
 int levelToOutput(int level) {
